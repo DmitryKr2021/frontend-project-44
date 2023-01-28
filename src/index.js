347a4c2ecc7a27c2f9dd21e3d54df4478dbe50f2
Dmitry <krdvmail@mail.ru>
2023-01-28 14:25:26 +0300

    fix index

--- src/index.js
+++ src/index.js
@@ -13,14 +13,8 @@ export default function startGames() {
   userName = askName();
 }
 
-export function checkAnswer(answers, functionName) {
-  // if (answers[0] === answers[1]) {
-// eslint-disable-next-line default-param-last
-// export function checkAnswer([userAnswer, correctAnswer] = answers, functionName) {
-  let userAnswer = '';
-  let correctAnswer = '';
-  [userAnswer, correctAnswer] = answers;
-  if (userAnswer === correctAnswer) {
+/* export function checkAnswer(answers, functionName) {
+  if (answers[0] === answers[1]) {
     console.log('Correct!');
     counter += 1;
     if (counter === maxCount) {
@@ -40,4 +34,29 @@ export function checkAnswer(answers, functionName) {
 Let's try again, ${userName}!`);
   }
   return true;
+} */
+
+export function checkAnswer(answers, functionName) {
+  if (answers[0] === answers[1]) {
+    console.log('Correct!');
+    counter += 1;
+  } else {
+    console.log(`'${answers[0]}' is wrong answer; (. Correct answer was '${answers[1]}'
+Let's try again, ${userName}!`);
+    return;
+  }
+
+  if (counter === maxCount) {
+    console.log(`Congratulations, ${userName}!`);
+  } else {
+    switch (functionName) {
+      case 'evenNumber': checkAnswer(isEvenNumber(), 'evenNumber'); break;
+      case 'commonDivisor': checkAnswer(isCommonDivisor(), 'commonDivisor'); break;
+      case 'primeNumber': checkAnswer(isPrimeNumber(), 'primeNumber'); break;
+      case 'progression': checkAnswer(isInProgression(), 'progression'); break;
+      case 'calculator': checkAnswer(calculator(), 'calculator'); break;
+      default:
+    }
+  }
+  // return true;
 }
