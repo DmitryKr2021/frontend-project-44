b849348de490a96fdad650c3a7798dd6c7d5d196
Dmitry <krdvmail@mail.ru>
2023-01-28 13:51:10 +0300

    fix ptimeNumber

--- src/games/primeNumber.js
+++ src/games/primeNumber.js
@@ -2,11 +2,17 @@ import readlineSync from 'readline-sync';
 
 export default function isPrimeNumber() {
   function isPrime(arg) {
-    if (arg % 2 === 0) { return false; }
+    /* if (arg % 2 === 0) { return false; }
     for (let divisor = 3; divisor < Math.round(arg / 2); divisor += 2) {
       if (arg % divisor === 0) { return false; }
     }
-    return true;
+    return true; */
+
+    let result = arg % 2;
+    for (let divisor = 3; divisor < Math.round(arg / 2); divisor += 2) {
+      result *= arg % divisor;
+    }
+    return result !== 0;
   }
   const testNumber = Math.ceil(Math.random() * 20);
   console.log(`Question: ${testNumber}`);
