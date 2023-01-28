b9f98ee491386cb5d3c19859a433492f7abd0d09
Dmitry <krdvmail@mail.ru>
2023-01-28 15:13:36 +0300

    fix primeNumber

--- src/games/primeNumber.js
+++ src/games/primeNumber.js
@@ -2,15 +2,13 @@ import readlineSync from 'readline-sync';
 
 export default function isPrimeNumber() {
   function isPrime(arg) {
-    if (arg === 1) { return false; }
-    if (arg === 2) { return true; }
     let result = arg % 2;
-    for (let divisor = 3; divisor < Math.round(arg / 2); divisor += 2) {
+    for (let divisor = 3; divisor < Math.round(arg / 3); divisor += 2) {
       result *= arg % divisor;
     }
     return result !== 0;
   }
-  const testNumber = Math.ceil(Math.random() * 50);
+  const testNumber = Math.ceil(Math.random() * 50) + 2;
   console.log(`Question: ${testNumber}`);
   const userAnswer = readlineSync.question('Your answer: ');
   const correctAnswer = isPrime(testNumber) ? 'yes' : 'no';
