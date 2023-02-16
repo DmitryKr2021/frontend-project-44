6663736e95589fd8e2386a18c611dc740d6c1125
Dmitry <krdvmail@mail.ru>
2023-02-16 14:59:49 +0300

    add bin

---
+++ bin/brain-even.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import runEven from '../src/games/evenNumber.js';
+
+runEven();
---
+++ bin/brain-gcd.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import runGCD from '../src/games/commonDivisor.js';
+
+runGCD();
